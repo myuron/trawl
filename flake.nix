e4fe747662a880939045c22061f6d66c6c30ace3
myuron <myuron.dev@gmail.com>
2026-06-30 16:46:12 +0000

    chore: formatter and linter settings

--- flake.nix
+++ flake.nix
@@ -2,16 +2,30 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
   outputs =
-    { self, nixpkgs, flake-utils }:
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      treefmt-nix,
+    }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in
       {
+        formatter = treefmt-nix.lib.mkWrapper pkgs {
+          projectRootFile = "flake.nix";
+          programs = {
+            nixfmt.enable = true;
+            gofmt.enable = true;
+            golangci-lint.enable = true;
+          };
+        };
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             go
